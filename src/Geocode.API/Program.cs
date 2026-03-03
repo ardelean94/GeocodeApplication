7c07d27e062904e702ff6ceb5e4638bb45f7090e
Alexandru Ardelean <ardeleanalexdaniel@yahoo.com>
2026-03-03 12:42:00 +0200

    Add and configure dependencies for DynamoDB

--- src/Geocode.API/Program.cs
+++ src/Geocode.API/Program.cs
@@ -1,7 +1,11 @@
+using Geocode.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+builder.Services.AddDatabase();
+builder.Services.AddApplication(builder.Configuration);
 
 var app = builder.Build();
 
--- src/Geocode.Application/ApplicationServiceCollectionExtensions.cs
+++ src/Geocode.Application/ApplicationServiceCollectionExtensions.cs
@@ -1,15 +1,19 @@
-﻿using Amazon.DynamoDBv2.DataModel;
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
 using FluentValidation;
 using Geocode.Application.Repository;
 using Geocode.Application.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Geocode.Application;
 
 public static class ApplicationServiceCollectionExtensions
 {
-    public static IServiceCollection AddApplication(this IServiceCollection services)
+    public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration config)
     {
+        var awsOptions = config.GetAWSOptions();
+        services.AddDefaultAWSOptions(awsOptions);
         services.AddSingleton<IGeocodeService, GeocodeService>();
         services.AddValidatorsFromAssemblyContaining<IApplicationMarker>(ServiceLifetime.Transient);
         return services;
@@ -17,8 +21,9 @@ public static class ApplicationServiceCollectionExtensions
 
     public static IServiceCollection AddDatabase(this IServiceCollection services)
     {
+        services.AddAWSService<IAmazonDynamoDB>();
         services.AddSingleton<IGeocodeRepository, GeocodeRepository>();
-        services.AddScoped<IDynamoDBContext, DynamoDBContext>();
+        services.AddSingleton<IDynamoDBContext, DynamoDBContext>();
         return services;
     }
 }
