0998aca41a61fd596e5c650ec971d25afc1c0778
Alexandru Ardelean <ardeleanalexdaniel@yahoo.com>
2026-03-10 23:00:51 +0200

    Enable problem details for consistent and informative error responses.

--- src/Geocode.API/Program.cs
+++ src/Geocode.API/Program.cs
@@ -3,12 +3,14 @@ using Geocode.Application;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddProblemDetails();
 builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 builder.Services.AddDatabase();
 builder.Services.AddApplication(builder.Configuration);
 
 var app = builder.Build();
 
+app.UseStatusCodePages();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 
