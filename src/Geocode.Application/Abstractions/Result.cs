68dca49b2d579563a7a7f3f71609af7ab971d0ba
Alexandru Ardelean <ardeleanalexdaniel@yahoo.com>
2026-03-19 17:27:52 +0200

    Add Result pattern for better testability.

---
+++ src/Geocode.Application/Abstractions/Result.cs
@@ -0,0 +1,39 @@
+﻿namespace Geocode.Application.Abstractions;
+
+public class Result
+{
+    protected Result(bool isSuccess, Error error)
+    {
+        if (isSuccess && error != Error.None ||
+            !isSuccess && error == Error.None)
+            throw new InvalidOperationException();
+
+        this.IsSuccess = isSuccess;
+        this.Error = error;
+    }
+
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public Error Error { get; }
+
+    public static Result Success() => new(true, Error.None);
+    public static Result Failure(Error error) => new(false, error);
+    public static Result<TValue> Success<TValue>(TValue item) => new(item, true, Error.None);
+    public static Result<TValue> Failure<TValue>(Error error) => new(default, false, error);
+}
+
+public sealed class Result<TValue> : Result
+{
+    private readonly TValue? _value;
+
+    public TValue Value => IsSuccess
+        ? _value!
+        : throw new InvalidOperationException("The value of a failure result cannot be accessed.");
+
+
+    public Result(TValue? value, bool isSuccess, Error error)
+        : base(isSuccess, error) 
+    {
+        this._value = value;
+    }
+}
---
+++ src/Geocode.Application/Extensions/AddressErrors.cs
@@ -0,0 +1,32 @@
+﻿using Geocode.Application.Abstractions;
+
+namespace Geocode.Application.Extensions;
+
+public static class AddressErrors
+{
+    const string usAddressPattern = "{house_number} {street}, {city}, {state_code} {zip_code}, {country}";
+
+    public static readonly Error NullOrEmpty = new(
+        "Address.NullOrEmpty",
+        "Address cannot be null or empty.");
+
+    public static readonly Error InvalidAddress = new(
+        "Address.InvalidAddress",
+        $"The address must follow the US address pattern: {usAddressPattern}.");
+
+    public static readonly Error InvalidHouseOrStreet = new(
+        "Address.InvalidHouseOrStreet",
+        $"The house or street are invalid, please follow the US address pattern: {usAddressPattern}.");
+
+    public static readonly Error InvalidHouseNumber = new(
+        "Address.InvalidHouseNumber",
+        "The house number must be in number format.");
+
+    public static readonly Error InvalidStateOrZipcode = new(
+        "Address.InvalidStateOrZipcode",
+        $"The state or zip code are invalid, please follow the US address pattern: {usAddressPattern}.");
+
+    public static readonly Error InvalidZipcode = new(
+        "Address.InvalidZipcode",
+        "The zipcode must be in number format.");
+}
